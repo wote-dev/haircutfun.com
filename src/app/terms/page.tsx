6f311c893dc1aa33afd09c13dc6a57a73013c028
whispersoftheelders <205708280+wote-dev@users.noreply.github.com>
2025-09-25 11:01:58 +1000

    Update page.tsx

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Metadata } from 'next'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
